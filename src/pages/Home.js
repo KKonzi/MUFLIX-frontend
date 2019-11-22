830368e274c3db67251734731462134bf0ced1d9
KKonzi <whalswl97@khu.ac.kr>
2019-11-22 13:51:53 +0900

    ui: Music 영역 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -94,6 +94,40 @@ const NetflixImage = styled.img`
   -ms-user-select: none;
 `;
 
+const MusicArea = styled.div`
+   width: 1200px;
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   margin-top: 50px;
+`;
+
+const MusicHeader = styled.div`
+   width: 980px;
+`;
+
+const MusicImageArea = styled.div`
+   width: 1000px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   flex-wrap: wrap;
+`;
+
+const MusicImage = styled.img`
+  width: 180px;
+  height: 180px;
+  margin: 10px;
+  cursor: pointer;
+  user-drag: none; 
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+`;
+
 const Home = () => {
   const movieImgSrc = [
     `http://image2.megabox.co.kr/mop/poster/2019/37/FC0155-CBED-48D6-B4F8-0F686D79CE86.large.jpg`,
@@ -111,6 +145,14 @@ const Home = () => {
     `https://occ-0-993-988.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABSdRt7iG0WbUmoBonVmSPfkA0DNOubx7uLK7vJr5Ejs26p1dkXpmE-FpPhD9PcONoGH-daBwIJeHyFzvAyMF-9sjMD4.webp?r=384`,
     `https://occ-0-993-988.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABeveVU0HijfVOvPWZOst2uI30wcy4c3gpc5ZDKPF2QxlO8PpTP4nPx8gVhHm8GZmdQma40mBsSZjRoeCtIT0VvuVeCc.webp?r=dff`,
     `https://occ-0-993-988.1.nflxso.net/dnm/api/v6/0DW6CdE4gYtYx8iy3aj8gs9WtXE/AAAABTG1sKQgLeIUK0kc2GUfEfnEMeHhlwouZs2C8Qi-ovrtWyVbz-bNt3SNVfUGkkFXngk07tuVHhgt7crlbWq7Dua3CsHcTfxvM5lNQcmB-F_3t2LFUXLygACD401a.jpg?r=89f`,
+  ];
+
+  const musicImgSrc = [
+    `https://musicmeta-phinf.pstatic.net/album/000/439/439225.jpg?type=r204Fll&v=20190523034503`,
+    `https://musicmeta-phinf.pstatic.net/album/003/056/3056861.jpg?type=r204Fll&v=20191111230709`,
+    `https://musicmeta-phinf.pstatic.net/album/000/411/411459.jpg?type=r204Fll&v=20191107011311`,
+    `https://musicmeta-phinf.pstatic.net/album/001/772/1772995.jpg?type=r204Fll&v=20190606213218`,
+    `https://musicmeta-phinf.pstatic.net/album/002/566/2566992.jpg?type=r204Fll&v=20191108013210`,
   ];
 
   return (
@@ -138,6 +180,17 @@ const Home = () => {
             )}
           </NetflixImageArea>
         </NetflixArea>
+
+        <MusicArea>
+          <MusicHeader>
+            당신이 본 영화의 OST, 들어 보셨나요?
+          </MusicHeader>
+          <MusicImageArea>
+            {musicImgSrc.map(imgSrc =>
+              <MusicImage src={imgSrc}/>
+            )}
+          </MusicImageArea>
+        </MusicArea>
       </Container>
 
 
