2aad602428b4c0200990c71d889b5c0674577b4d
KKonzi <whalswl97@khu.ac.kr>
2019-12-02 22:14:35 +0900

    feat: Header 컴포넌트 분리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,94 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import {logo,leftArrow,rightArrow} from '../images'
+import {leftArrow,rightArrow} from '../images';
+import Header from './components/Header';
 
 const Screen = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
   font-family: 'Noto Sans KR', sans-serif;
-`;
-
-const Header = styled.div`
-  position: absolute;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: 60px;
-  box-shadow: 3px 3px 6px 0 rgba(0, 0, 0, 0.1);
-  background-color: #ffffff;
-  
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 10;
-`;
-
-const HeaderContent = styled.div`
-  width: 1050px;
-  //background-color: red;  
-  
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const LogoImage = styled.img.attrs({
-  src: logo
-  })`
-  height: 35px;
-`
-
-const LoginForm = styled.div`
-  display: flex;
-`
-
-const IDInputBox = styled.input.attrs({
-  type: 'text'
-})`
-  height: 20px;
-  width: 110px;
-  margin-left: 15px;
-
-  border: 1px solid lightgrey;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-`;
-
-const PWInputBox = styled.input.attrs({
-  type: 'password'
-})`
-  height: 20px;
-  width: 110px;
-  margin-left: 15px;
-  border: 1px solid lightgrey;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-`;
-
-const LoginButton = styled.div`
-  margin-left: 15px;
-  height: 18px; 
-  width: 50px;
-  border: 1px solid lightgrey;
-  border-radius: 5px;
-  
-  padding-top: 2px;
-  letter-spacing: -0.7px;
-  
-  display-flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  
-  font-size: 11px;
-  color: #2e2e2e;
-  
-  cursor: pointer;
 `;
 
 const Container = styled.div`
@@ -321,16 +240,7 @@ const Home = () => {
 
   return (
     <Screen>
-      <Header>
-        <HeaderContent>
-          <LogoImage/>
-          <LoginForm>
-            <IDInputBox type="password" placeholder={"ID"}/>
-            <PWInputBox type="text" placeholder={"PASSWORD"}/>
-            <LoginButton>로그인</LoginButton>
-          </LoginForm>
-       </HeaderContent>
-      </Header>
+      <Header/>
       <Container>
         <MovieArea>
           <MovieHeader>현재 상영중인 영화</MovieHeader>
